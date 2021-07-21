d899ea0ead1b79792d9039158608299898ae5c3a
elaiv <Elaivelite@gmail.com>
2021-07-21 22:13:26 +0300

    Карта!

--- projects/eco-ui/src/app/app.component.ts
+++ projects/eco-ui/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import * as L from 'leaflet';
 @Component({
   selector: 'eco-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'eco-ui';
+export class AppComponent implements OnInit {
+	ngOnInit(): void {
+		this.initMap();
+	}
+  constructor() {
+
+	}
+
+	initMap() {
+		const map = L.map('map').setView([51.505, -0.09], 13);
+		L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+			subdomains:['mt0','mt1','mt2','mt3'],
+			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
+	}
 }
