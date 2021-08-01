86963e2cfbe70a7a0fa80fb1f2f3423d5c559496
Vlad <vladdaking@gmail.com>
2021-08-01 18:06:07 +0300

    add themes support

--- projects/eco-ui/src/app/app.component.ts
+++ projects/eco-ui/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'eco-ui';
+
+  changeTheme(e: any): void {
+    e.target.checked
+      ? document.body.setAttribute('cap-theme', 'dark')
+      : document.body.removeAttribute('cap-theme');
+  }
 }
--- projects/eco-ui/src/app/app.module.ts
+++ projects/eco-ui/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
